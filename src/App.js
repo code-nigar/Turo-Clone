4923fb1a7fff49f976696195ce39ed8990e1007d
Sharjeel Sultan <sharjeelsultan27@gmail.com>
2022-11-24 16:01:14 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNav from './components/MyNav/MyNav';
+import SimpleSlider from './components/Slider/Slider';
+import CardOverlay from './components/CardOverlay/CardOverlay';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNav/>
+      <CardOverlay/>
+      <h1>Find your drive</h1>
+      <h4>Explore the world's largest car sharing market place</h4>
+      <div>
+        <p>Browse by make</p>
+        
+      </div>
     </div>
   );
 }
